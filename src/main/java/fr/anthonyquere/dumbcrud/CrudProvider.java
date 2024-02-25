6fdebeaeb5eb35da3a9b11506ce844c7c94d5f3e
anthony <quereantho@gmail.com>
2024-02-25 20:31:38 +0100

    :tada: First commit

---
+++ src/main/java/fr/anthonyquere/dumbcrud/CrudProvider.java
@@ -0,0 +1,9 @@
+package fr.anthonyquere.dumbcrud;
+
+import java.util.List;
+
+public interface CrudProvider<Domain> {
+    void add(Domain domain) throws Exception;
+
+    List<Domain> list() throws Exception;
+}
