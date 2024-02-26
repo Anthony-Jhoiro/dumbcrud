68e0cd9a819dab3e62562e208a26e23dc5eb905d
anthony <quereantho@gmail.com>
2024-02-26 14:42:23 +0100

    :construction_worker: Update deployment config

--- src/main/java/fr/anthonyquere/dumbcrud/CrudProvider.java
+++ src/main/java/fr/anthonyquere/dumbcrud/CrudProvider.java
@@ -2,6 +2,10 @@ package fr.anthonyquere.dumbcrud;
 
 import java.util.List;
 
+/**
+ * Describes a providers that handles stocking the Domain object
+ * @param <Domain> the object that you want to manage with your CRUD
+ */
 public interface CrudProvider<Domain> {
     void add(Domain domain) throws Exception;
 
